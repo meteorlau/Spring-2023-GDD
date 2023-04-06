31c9c464fee7570b6d6feb733ad61aa7575b549b
lucywan <31701862+lucywan@users.noreply.github.com>
2023-04-06 15:45:06 -0700

    mangosteen initial

---
+++ Assets/Scripts/Characters/Enemy/Close Range/EnemyMangosteen.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMangosteen : EnemyBase
+{
+    // Start is called before the first frame update
+
+    [SerializeField] private GameObject hitVFX = null;
+    [SerializeField] private int explosionDamage = 5;
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Color pink = new Color(0.0f, 0.6f, 0.3f, 1.0f);
+            collision.gameObject.GetComponent<Renderer>().material.color = pink;
+            GameObject vfx = Instantiate(hitVFX, transform.position, Quaternion.identity);
+            vfx.layer = gameObject.layer;
+            vfx.GetComponent<SpriteRenderer>().sortingLayerName = GetComponent<SpriteRenderer>().sortingLayerName;
+            Destroy(vfx, 5f);
+            Destroy(transform.parent.gameObject);
+        }
+    }
+
+    public override string ToString()
+    {
+        return "Mangosteen";
+    }
+}
