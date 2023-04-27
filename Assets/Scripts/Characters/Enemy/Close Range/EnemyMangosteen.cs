26e39e9cefed671d9a94f4f5cdd618ca5e52ecd2
lucywan <31701862+lucywan@users.noreply.github.com>
2023-04-26 23:33:02 -0700

    everything added

--- Assets/Scripts/Characters/Enemy/Close Range/EnemyMangosteen.cs
+++ Assets/Scripts/Characters/Enemy/Close Range/EnemyMangosteen.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class EnemyMangosteen : EnemyBase
 {
     // Start is called before the first frame update
+    public GameObject circlePrefab;
+    private Camera cam;
 
     [SerializeField] private GameObject hitVFX = null;
     protected override void Start()
     {
         base.Start();
+        cam = Camera.main;
     }
 
     protected override void Update()
@@ -19,70 +22,80 @@ public class EnemyMangosteen : EnemyBase
 
     public Material material;
 
-    public void splatter(GameObject player)
+    public void splatter()
     {
 
-        GameObject square = new GameObject("Square");
-        square.layer = player.layer;
-        MeshRenderer meshRenderer = square.AddComponent<MeshRenderer>();
+        Vector3 dimensions = cam.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, cam.nearClipPlane));
 
-        MeshFilter meshFilter = square.AddComponent<MeshFilter>();
-
-        Mesh mesh = new Mesh();
-
-        Vector3[] vertices = new Vector3[4]
+        for (int i = 0; i < 10; i++)
         {
-            new Vector3(0, 1),
-            new Vector3(1, 1),
-            new Vector3(0, 0),
-            new Vector3(1, 0)
-        };
-        mesh.vertices = vertices;
+            int randX = Random.Range(0, (int) dimensions.x) * 2;
+            int randY = Random.Range(0, (int) dimensions.y) * 2;
+            
+            Instantiate(circlePrefab, new Vector3(randX, randY, 0), Quaternion.identity);
+        }
+        
 
-        int[] tris = new int[6]
-        {
-            // lower left triangle
-            0, 1, 2,
-            // upper right triangle
-            2, 1, 3
-        };
-        mesh.triangles = tris;
+        ////circle prefab: instantiate
+        //GameObject circle = new GameObject("Circle");
+        //circle.layer = player.layer;
 
-        Vector3[] normals = new Vector3[4]
-        {
-            -Vector3.forward,
-            -Vector3.forward,
-            -Vector3.forward,
-            -Vector3.forward
-        };
-        mesh.normals = normals;
+        //SpriteRenderer spriteRenderer = gameObject.AddComponent<SpriteRenderer>();
+        //spriteRenderer.sprite = Resources.Load<Sprite>("");
+        //spriteRenderer.color = Color.black;
 
-        Vector2[] uv = new Vector2[4]
-        {
-            new Vector2(0, 1),
-            new Vector2(1, 1),
-            new Vector2(0, 0),
-            new Vector2(1, 0),
-        };
-        mesh.uv = uv;
-        Color[] colors = new Color[vertices.Length];
+        //MeshRenderer meshRenderer = square.AddComponent<MeshRenderer>();
 
-        for (int i = 0; i < vertices.Length; i++)
-            colors[i] = Color.Lerp(Color.red, Color.green, vertices[i].y);
+        //MeshFilter meshFilter = square.AddComponent<MeshFilter>();
 
-        // assign the array of colors to the Mesh.
-        mesh.colors = colors;
+        //Mesh mesh = new Mesh();
 
-        // assign the array of colors to the Mesh.
-        meshFilter.mesh = mesh;
-        Debug.Log(mesh);
-        //meshRenderer.material = material;
+        //Vector3[] vertices = new Vector3[4]
+        //{
+        //    new Vector3(0, 1),
+        //    new Vector3(1, 1),
+        //    new Vector3(0, 0),
+        //    new Vector3(1, 0)
+        //};
+        //mesh.vertices = vertices;
 
-        //Color pink = new Color(0.0f, 0.6f, 0.3f, 1.0f);
-        //square.GetComponent<Renderer>().material.color = pink;
+        //int[] tris = new int[6]
+        //{
+        //    // lower left triangle
+        //    0, 1, 2,
+        //    // upper right triangle
+        //    2, 1, 3
+        //};
+        //mesh.triangles = tris;
 
+        //Vector3[] normals = new Vector3[4]
+        //{
+        //    -Vector3.forward,
+        //    -Vector3.forward,
+        //    -Vector3.forward,
+        //    -Vector3.forward
+        //};
+        //mesh.normals = normals;
+
+        //Vector2[] uv = new Vector2[4]
+        //{
+        //    new Vector2(0, 1),
+        //    new Vector2(1, 1),
+        //    new Vector2(0, 0),
+        //    new Vector2(1, 0),
+        //};
+        //mesh.uv = uv;
+        //Color[] colors = new Color[vertices.Length];
+
+        //for (int i = 0; i < vertices.Length; i++)
+        //    colors[i] = Color.Lerp(Color.red, Color.green, vertices[i].y);
+
+        //// assign the array of colors to the Mesh.
+        //mesh.colors = colors;
+
+        //// assign the array of colors to the Mesh.
+        //meshFilter.mesh = mesh;
     }
-
 
     public void OnCollisionEnter2D(Collision2D collision)
     {
@@ -93,7 +106,7 @@ public class EnemyMangosteen : EnemyBase
             ////splatter();
             //collision.gameObject.GetComponent<Renderer>().material.color = pink;
 
-            splatter(collision.gameObject);
+            splatter();
 
             //GameObject vfx = Instantiate(hitVFX, transform.position, Quaternion.identity);
             //vfx.layer = gameObject.layer;
