f43a21897454492dcb0284e1452512af7c5d8b0d
bumsooreal <123034534+bumsooreal@users.noreply.github.com>
2023-04-30 14:04:40 -0700

    Durian
    Seems to be really buggy right now

--- Assets/Scripts/Characters/Enemy/EnemyDurian.cs
+++ Assets/Scripts/Characters/Enemy/EnemyDurian.cs
@@ -4,40 +4,54 @@ using UnityEngine;
 
 public class EnemyDurian : EnemyBase
 {
-    protected float damage; // damage per tick
-    protected float range;
-    protected float rate; // Number of hits per second
-    protected float threshold = 0.7; // Slows player when under 70% health? Or is there a way to see if it's been damaged
-    protected bool harmed;
+    [SerializeField] protected int damage; // damage per tick
+    [SerializeField] protected float range;
+    [SerializeField] protected float rate; // Number of hits per second
+    [SerializeField] protected double threshold; // Slows player when under 70% health? Or is there a way to see if it's been damaged
+    [SerializeField] protected bool harmed;
+    [SerializeField] protected GameObject poisonCloud;
+    [SerializeField] private double slowMultiplier;
 
     // Start is called before the first frame update
-    override void Start() {
+    protected override void Start() {
         base.Start();
         // Set up properties for AoE, arbitrary numbers rn
-        damage = 5;
-        range = 80;
+        damage = 4;
+        range = 7;
         rate = 1;
+        threshold = 0.7;
+        slowMultiplier = 0.8;
         harmed = false;
+
+        // Not sure if this works
+        poisonCloud = Instantiate(GameObject.Find("PoisonCloud"));
     }
 
-    // Update is called once per frame
-    override void Update() {
+    protected override void Update() {
         base.Update();
-        if (GetComponent<Health>().) { // measure health compared to threshold OR see it's been damaged
-
-        }
-        if (playerInRange()) {
-            applyEffects();
-        }
-    }
-
-    private bool playerInRange() {
-        // Returns if player is in range of AoE
+        
+        poisonCloud.GetComponent<Transform>().position = base.GetComponent<Transform>().position;
+        harmed = (base.GetComponent<Health>().GetCurrentHealth() / base.GetComponent<Health>().GetMaxHealth()) < threshold;
     }
 
     private void applyEffects() {
-        // damages target player
-        // if harmed, also slows player
+        player.GetComponent<Health>().TakeDamage(damage);
+        if (harmed) {
+            Vector3 var = player.GetComponent<Rigidbody2D>().velocity;
+            player.GetComponent<Rigidbody2D>().velocity = var * slowMultiplier;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            applyEffects();
+            GameObject vfx = Instantiate(hitVFX, transform.position, Quaternion.identity);
+            vfx.layer = gameObject.layer;
+            Destroy(vfx, 5f);
+            Destroy(transform.parent.gameObject);
+        }
     }
 
     
